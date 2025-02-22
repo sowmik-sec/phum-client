5bd98459706f159cc9854b31d60b0948a33e3b33
sowmik-sec <sowmik.sec@gmail.com>
2025-02-22 19:47:38 +0600

    Fixing the layout and setting up basic sidebar options

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,26 +1,34 @@
-import React from "react";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const items: MenuProps["items"] = [
+  {
+    key: "fds",
+    label: "Dashboard",
+  },
+  {
+    key: "fdsa",
+    label: "Profile",
+  },
+  {
+    key: "fdss",
+    label: "User Management",
+    children: [
+      {
+        key: "fdssd",
+        label: "Create Admin",
+      },
+      {
+        key: "fdsafs",
+        label: "Create Student",
+      },
+    ],
+  },
+];
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout style={{ height: "100vh" }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -31,7 +39,17 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
-        <div className="demo-logo-vertical" />
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            justifyItems: "center",
+          }}
+        >
+          <h1 style={{ height: "20px" }}>PH Uni</h1>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
