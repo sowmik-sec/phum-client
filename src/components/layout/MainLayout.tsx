34bd360037e738b1bf1a307ee112ccaab92a9f35
sowmik-sec <sowmik.sec@gmail.com>
2025-02-22 20:38:23 +0600

    Absolute path, relative path, outlet, and Layout layer.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -66,7 +67,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            <h1>The main content</h1>
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
