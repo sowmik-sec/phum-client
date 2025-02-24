eed2e843c63c27178cfe2910c195d7966bb600bb
sowmik-sec <sowmik.sec@gmail.com>
2025-02-24 17:23:32 +0600

    Integrate redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
@@ -16,3 +24,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
