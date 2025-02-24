6bafba61ecf1b2bd3bac23dc474c70dbc4f17477
sowmik-sec <sowmik.sec@gmail.com>
2025-02-24 17:31:56 +0600

    Serializable and non-serializable objects

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "auth",
@@ -17,7 +26,11 @@ export const store = configureStore({
     auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
