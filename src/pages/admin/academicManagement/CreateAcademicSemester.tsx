99fe1380e4e2cf6f94cf896c7974b768f5b8eb44
sowmik-sec <sowmik.sec@gmail.com>
2025-03-15 11:47:49 +0600

    Refactor Form component

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,17 +1,24 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHInput from "../../../components/form/PHInput";
 import PHForm from "../../../components/form/PHForm";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="name" />
+          <PHInput type="text" name="year" label="year" />
+          <PHSelect label="name" name="" options={} />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
