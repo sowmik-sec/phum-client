6144cf93f9791310370aad4a570d44684bcf12ea
sowmik-sec <sowmik.sec@gmail.com>
2025-03-17 17:54:33 +0600

    create some static faculty options

---
+++ src/constants/faculty.ts
@@ -0,0 +1,21 @@
+export const facultyOptions = [
+  {
+    value: "Faculty of Life Science and Engineering",
+    label: "Faculty of Life Science and Engineering",
+  },
+  { value: "Faculty of Business", label: "Faculty of Business" },
+  { value: "Faculty of Arts", label: "Faculty of Arts" },
+  { value: "Faculty of Science", label: "Faculty of Science" },
+  { value: "Faculty of Law", label: "Faculty of Law" },
+  { value: "Faculty of Medicine", label: "Faculty of Medicine" },
+  { value: "Faculty of Education", label: "Faculty of Education" },
+  { value: "Faculty of Social Sciences", label: "Faculty of Social Sciences" },
+  {
+    value: "Faculty of Computer Science",
+    label: "Faculty of Computer Science",
+  },
+  {
+    value: "Faculty of Environmental Studies",
+    label: "Faculty of Environmental Studies",
+  },
+];
