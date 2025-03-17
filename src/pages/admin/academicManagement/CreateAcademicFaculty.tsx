a5646ddfb8a02f0f40e01222fcc088737903eb9a
sowmik-sec <sowmik.sec@gmail.com>
2025-03-17 17:57:46 +0600

    create academic faculty form and make it functional

--- src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -1,5 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import { facultyOptions } from "../../../constants/faculty";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicFacultySchema } from "../../../schemas/academicFaculty.schema";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useAddAcademicFacultyMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+import { TResponseRedux } from "../../../types";
+import { TAcademicFaculty } from "../../../types/academicManagement.type";
+
 const CreateAcademicFaculty = () => {
-  return <div>create academic faculty</div>;
+  const [createAcademicFaculty] = useAddAcademicFacultyMutation();
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const academicFacultyData = {
+      name: data?.name,
+    };
+    console.log(academicFacultyData);
+    const toastId = toast.loading("Creating...");
+    try {
+      const res = (await createAcademicFaculty(
+        academicFacultyData
+      )) as TResponseRedux<TAcademicFaculty>;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicFacultySchema)}
+        >
+          <PHSelect
+            label="Academic Faculty"
+            name="name"
+            options={facultyOptions}
+          />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateAcademicFaculty;
