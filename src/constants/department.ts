68ddc83cd93fc21303e8141bef174cce1f6bbc1d
sowmik-sec <sowmik.sec@gmail.com>
2025-03-18 16:58:11 +0600

    create academic department options

---
+++ src/constants/department.ts
@@ -0,0 +1,39 @@
+export const academicDepartmentOptions: { value: string; label: string }[] = [
+  { value: "Department of L1", label: "Department of L1" },
+  {
+    value: "Department of Artificial Intelligence",
+    label: "Department of Artificial Intelligence",
+  },
+  {
+    value: "Department of Machine Learning",
+    label: "Department of Machine Learning",
+  },
+  {
+    value: "Department of Cybersecurity",
+    label: "Department of Cybersecurity",
+  },
+  {
+    value: "Department of Software Engineering",
+    label: "Department of Software Engineering",
+  },
+  {
+    value: "Department of Computer Networks",
+    label: "Department of Computer Networks",
+  },
+  {
+    value: "Department of Cloud Computing",
+    label: "Department of Cloud Computing",
+  },
+  {
+    value: "Department of Human-Computer Interaction",
+    label: "Department of Human-Computer Interaction",
+  },
+  {
+    value: "Department of Computer Vision",
+    label: "Department of Computer Vision",
+  },
+  {
+    value: "Department of Natural Language Processing",
+    label: "Department of Natural Language Processing",
+  },
+];
