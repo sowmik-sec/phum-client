00721c2d1d9df3e65d3d81117443f6c2b095d7a2
sowmik-sec <sowmik.sec@gmail.com>
2025-03-18 12:26:58 +0600

    create academic faculty table

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,5 +1,44 @@
+import { Button, Table, TableColumnsType } from "antd";
+import { useGetAllAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { TAcademicFaculty } from "../../../types/academicManagement.type";
+
+export type TTableData = Pick<TAcademicFaculty, "name">;
+
 const AcademicFaculty = () => {
-  return <div>academic faculty</div>;
+  const {
+    data: academicFaculties,
+    isLoading,
+    isFetching,
+  } = useGetAllAcademicFacultiesQuery(undefined);
+  console.log(academicFaculties);
+  console.log({ isLoading, isFetching });
+  const tableData = academicFaculties?.data?.map(
+    ({ _id, name }: TAcademicFaculty) => ({
+      key: _id,
+      name,
+    })
+  );
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <Table loading={isFetching} columns={columns} dataSource={tableData} />
+  );
 };
 
 export default AcademicFaculty;
