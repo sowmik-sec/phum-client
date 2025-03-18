ca5cae59144d5b6840dabb500cc85465c67664e0
sowmik-sec <sowmik.sec@gmail.com>
2025-03-18 12:26:17 +0600

    edit faculty options

--- src/constants/faculty.ts
+++ src/constants/faculty.ts
@@ -3,19 +3,12 @@ export const facultyOptions = [
     value: "Faculty of Life Science and Engineering",
     label: "Faculty of Life Science and Engineering",
   },
-  { value: "Faculty of Business", label: "Faculty of Business" },
-  { value: "Faculty of Arts", label: "Faculty of Arts" },
+  { value: "Faculty of Web Development", label: "Faculty of Web Development" },
+  { value: "Faculty of Engineering", label: "Faculty of Engineering" },
   { value: "Faculty of Science", label: "Faculty of Science" },
-  { value: "Faculty of Law", label: "Faculty of Law" },
-  { value: "Faculty of Medicine", label: "Faculty of Medicine" },
-  { value: "Faculty of Education", label: "Faculty of Education" },
-  { value: "Faculty of Social Sciences", label: "Faculty of Social Sciences" },
+  { value: "Faculty of Programming", label: "Faculty of Programming" },
   {
     value: "Faculty of Computer Science",
     label: "Faculty of Computer Science",
-  },
-  {
-    value: "Faculty of Environmental Studies",
-    label: "Faculty of Environmental Studies",
   },
 ];
