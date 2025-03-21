a4dc42f889a18ab215c3237f04c1f9cd1712ea37
sowmik-sec <sowmik.sec@gmail.com>
2025-03-21 12:24:01 +0600

    Offered course data modification for UI

--- src/redux/features/student/studentCourseManagement.api.ts
+++ src/redux/features/student/studentCourseManagement.api.ts
@@ -28,15 +28,43 @@ const studentCourseApi = baseApi.injectEndpoints({
         };
       },
     }),
+    getAllEnrolledCourses: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
 
-    addStudent: builder.mutation({
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses/my-enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["offeredCourse"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    enrolCourse: builder.mutation({
       query: (data) => ({
-        url: "/users/create-student",
+        url: "/enrolled-courses/create-enrolled-course",
         method: "POST",
         body: data,
       }),
+      invalidatesTags: ["offeredCourse"],
     }),
   }),
 });
 
-export const { useGetAllOfferedCoursesQuery } = studentCourseApi;
+export const {
+  useGetAllOfferedCoursesQuery,
+  useEnrolCourseMutation,
+  useGetAllEnrolledCoursesQuery,
+} = studentCourseApi;
